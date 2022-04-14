3604cbef7254861b135f2389cca360ece59c00f4
Kristopher Wasserman <kristopherwasserman6@msn.com>
2022-04-14 12:51:44 -0500

    added apis

---
+++ BackEnd/SpaApi/Program.cs
@@ -0,0 +1,31 @@
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+
+builder.Services.AddControllers();
+// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+Log.Logger = new LoggerConfiguration().WriteTo.File("./logs/user.txt").CreateLogger();
+var configuration = new ConfigurationBuilder()
+    .SetBasePath(Directory.GetCurrentDirectory())
+    .AddJsonFile("appsettings.json")
+    .Build();
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+app.UseHttpsRedirection();
+
+app.UseAuthorization();
+
+app.MapControllers();
+
+app.Run();
--- BackEnd/SpaTest/Program.cs
+++ BackEnd/SpaTest/Program.cs
@@ -1,6 +1,11 @@
-﻿using SpaBL;
+﻿global using Serilog;
+using SpaBL;
 using SpaDl;
 using Model;
 
 
-Console.WriteLine("Hello, World!");
+Log.Logger = new LoggerConfiguration().WriteTo.File("./logs/user.txt").CreateLogger();
+var configuration = new ConfigurationBuilder()
+    .SetBasePath(Directory.GetCurrentDirectory())
+    .AddJsonFile("appsettings.json")
+    .Build();
